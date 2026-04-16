fe8a166a18bc7072e242bfd483b646d31a871545
zhuyiheng <1975675405@qq.com>
2026-04-16 13:41:16 +0800

    添加诗词应用后端代码：实体类、Mapper、Service、Controller

---
+++ target/classes/static/miniprogram/app.js
@@ -0,0 +1,18 @@
+App({
+  onLaunch() {
+    // 初始化云开发
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        env: 'prod-4gnit1gx2a365651',
+        traceUser: true
+      })
+    }
+  },
+
+  globalData: {
+    userInfo: null,
+    serverUrl: '' // 云托管无需配置，直接调用云函数
+  }
+})
